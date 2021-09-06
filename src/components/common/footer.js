0a763ac1d705bd13d27d7018777e2c47a6e485ef
Yash Kalaria <yash.kalaria@tntra.io>
2021-09-06 08:46:56 +0530

    Changed the copyright text.

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="footer">
-      <div>&copy; Sintellisys 2020</div>
+      <div>&copy; Yash Kalaria</div>
     </div>
   );
 };
