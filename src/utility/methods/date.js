37cecf34756ef9cdbd02389254878a69881b9ef4
Yash Kalaria <yash.kalaria@tntra.io>
2021-09-15 13:21:34 +0530

    Added duration plugin of dayjs.

--- src/utility/methods/date.js
+++ src/utility/methods/date.js
@@ -2,43 +2,44 @@ import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import updateLocale from 'dayjs/plugin/updateLocale';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
+import duration from 'dayjs/plugin/duration';
 import constants from '../../constants/constants';
 import i18nInstance from '../../setup/i18next';
 import { capitalize } from '../index';
 
-let locale = i18nInstance.language?.substring(0, 2);
+const locale = i18nInstance.language?.substring(0, 2);
 dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
 dayjs.extend(updateLocale);
+dayjs.extend(duration);
 dayjs.locale(locale);
 dayjs.updateLocale(
   locale,
   locale === 'en' ? constants.enDayJSLocaleObj : constants.jaDayJSLocaleObj,
 );
 
-const formatDate = (date) => date && dayjs(date).format('L');
+const formatDate = (date) => (date ? dayjs(date).format('L') : '');
 
-const formatTime = (time) => time && dayjs(time).format('LT');
+const formatTime = (time) => (time ? dayjs(time).format('LT') : '');
 
-const formatDateAndTime = (date) => date && dayjs(date).format('L LT');
+const formatDateAndTime = (date) => (date ? dayjs(date).format('L LT') : '');
 
-const formatDateBySpecifiedFormat = (date, format) =>
-  date && format && dayjs(date).format(format);
+const formatDateBySpecifiedFormat = (date, format) => (date ? format && dayjs(date).format(format) : '');
 
-const fromNow = (date) => dayjs(date).fromNow();
+const fromNow = (date) => (date ? dayjs(date).fromNow() : '');
 
-const toNow = (date) => dayjs(date).toNow();
+const toNow = (date) => (date ? dayjs(date).toNow() : '');
 
 const timeTo = (date) => {
   let returnValue = '-';
   if (date) {
-    let today = dayjs();
-    let checkingDate = dayjs(date);
-    let diff = dayjs.duration(checkingDate.diff(today));
+    const today = dayjs();
+    const checkingDate = dayjs(date);
+    const diff = dayjs.duration(checkingDate.diff(today));
 
-    let hoursDiff = parseInt(diff.asHours(), 10);
+    const hoursDiff = parseInt(diff.asHours(), 10);
 
-    let minutesDiff = parseInt(diff.asMinutes(), 10) % 60;
+    const minutesDiff = parseInt(diff.asMinutes(), 10) % 60;
 
     if (hoursDiff > 24) {
       returnValue = capitalize(dayjs(date).toNow(true));
